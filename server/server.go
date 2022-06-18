0d88400f2e7b2554828afd8d716217e21b5e7cb2
Craig Pastro <pastro.craig@gmail.com>
2022-06-17 20:29:30 -0700

    Add validation (#59)

--- server/server.go
+++ server/server.go
@@ -28,7 +28,22 @@ func NewServer(cache cache.Cache, storage storage.Storage, tracer trace.Tracer) 
 	}
 }
 
+type validator interface {
+	Validate() error
+}
+
+func validate[T validator](msg T) error {
+	if err := msg.Validate(); err != nil {
+		return connect.NewError(connect.CodeInvalidArgument, err)
+	}
+	return nil
+}
+
 func (s *server) Create(ctx context.Context, req *connect.Request[pb.CreateRequest]) (*connect.Response[pb.CreateResponse], error) {
+	if err := validate(req.Msg); err != nil {
+		return nil, err
+	}
+
 	c := commands.NewCreateCommand(s.Cache, s.Storage, s.Tracer)
 	res, err := c.Execute(ctx, req.Msg)
 	if err != nil {
@@ -39,6 +54,10 @@ func (s *server) Create(ctx context.Context, req *connect.Request[pb.CreateReque
 }
 
 func (s *server) Read(ctx context.Context, req *connect.Request[pb.ReadRequest]) (*connect.Response[pb.ReadResponse], error) {
+	if err := validate(req.Msg); err != nil {
+		return nil, err
+	}
+
 	c := commands.NewReadCommand(s.Cache, s.Storage, s.Tracer)
 	res, err := c.Execute(ctx, req.Msg)
 	if err != nil {
@@ -49,6 +68,10 @@ func (s *server) Read(ctx context.Context, req *connect.Request[pb.ReadRequest])
 }
 
 func (s *server) ReadAll(ctx context.Context, req *connect.Request[pb.ReadAllRequest]) (*connect.Response[pb.ReadAllResponse], error) {
+	if err := validate(req.Msg); err != nil {
+		return nil, err
+	}
+
 	c := commands.NewReadAllCommand(s.Cache, s.Storage, s.Tracer)
 	res, err := c.Execute(ctx, req.Msg)
 	if err != nil {
@@ -59,6 +82,10 @@ func (s *server) ReadAll(ctx context.Context, req *connect.Request[pb.ReadAllReq
 }
 
 func (s *server) Update(ctx context.Context, req *connect.Request[pb.UpdateRequest]) (*connect.Response[pb.UpdateResponse], error) {
+	if err := validate(req.Msg); err != nil {
+		return nil, err
+	}
+
 	c := commands.NewUpdateCommand(s.Cache, s.Storage, s.Tracer)
 	res, err := c.Execute(ctx, req.Msg)
 	if err != nil {
@@ -69,6 +96,10 @@ func (s *server) Update(ctx context.Context, req *connect.Request[pb.UpdateReque
 }
 
 func (s *server) Delete(ctx context.Context, req *connect.Request[pb.DeleteRequest]) (*connect.Response[pb.DeleteResponse], error) {
+	if err := validate(req.Msg); err != nil {
+		return nil, err
+	}
+
 	c := commands.NewDeleteCommand(s.Cache, s.Storage, s.Tracer)
 	res, err := c.Execute(ctx, req.Msg)
 	if err != nil {
