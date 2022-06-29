60392e53296ff16af6801a5a650bc945897d2375
Craig Pastro <pastro.craig@gmail.com>
2022-06-29 15:14:28 -0700

    Move commands back to server (#61)

--- server/server.go
+++ server/server.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com/bufbuild/connect-go"
 	"github.com/craigpastro/crudapp/cache"
-	"github.com/craigpastro/crudapp/commands"
+	"github.com/craigpastro/crudapp/errors"
 	pb "github.com/craigpastro/crudapp/internal/gen/crudapp/v1"
 	"github.com/craigpastro/crudapp/internal/gen/crudapp/v1/crudappv1connect"
 	"github.com/craigpastro/crudapp/storage"
+	"github.com/craigpastro/crudapp/telemetry"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type server struct {
@@ -44,13 +47,21 @@ func (s *server) Create(ctx context.Context, req *connect.Request[pb.CreateReque
 		return nil, err
 	}
 
-	c := commands.NewCreateCommand(s.Cache, s.Storage, s.Tracer)
-	res, err := c.Execute(ctx, req.Msg)
+	msg := req.Msg
+	userID := msg.GetUserId()
+	ctx, span := s.Tracer.Start(ctx, "Create", trace.WithAttributes(attribute.String("userID", userID)))
+	defer span.End()
+
+	record, err := s.Storage.Create(ctx, userID, msg.GetData())
 	if err != nil {
-		return nil, err
+		telemetry.TraceError(span, err)
+		return nil, errors.HandleStorageError(err)
 	}
 
-	return connect.NewResponse(res), nil
+	return connect.NewResponse(&pb.CreateResponse{
+		PostId:    record.PostID,
+		CreatedAt: timestamppb.New(record.CreatedAt),
+	}), nil
 }
 
 func (s *server) Read(ctx context.Context, req *connect.Request[pb.ReadRequest]) (*connect.Response[pb.ReadResponse], error) {
@@ -58,13 +69,30 @@ func (s *server) Read(ctx context.Context, req *connect.Request[pb.ReadRequest])
 		return nil, err
 	}
 
-	c := commands.NewReadCommand(s.Cache, s.Storage, s.Tracer)
-	res, err := c.Execute(ctx, req.Msg)
-	if err != nil {
-		return nil, err
+	msg := req.Msg
+	userID := msg.GetUserId()
+	postID := msg.GetPostId()
+	ctx, span := s.Tracer.Start(ctx, "Read", trace.WithAttributes(attribute.String("userID", userID), attribute.String("postID", postID)))
+	defer span.End()
+
+	var err error
+	record, ok := s.Cache.Get(ctx, userID, postID)
+	if !ok {
+		record, err = s.Storage.Read(ctx, userID, postID)
+		if err != nil {
+			telemetry.TraceError(span, err)
+			return nil, errors.HandleStorageError(err)
+		}
+		s.Cache.Add(ctx, userID, postID, record)
 	}
 
-	return connect.NewResponse(res), nil
+	return connect.NewResponse(&pb.ReadResponse{
+		UserId:    record.UserID,
+		PostId:    record.PostID,
+		Data:      record.Data,
+		CreatedAt: timestamppb.New(record.CreatedAt),
+		UpdatedAt: timestamppb.New(record.UpdatedAt),
+	}), nil
 }
 
 func (s *server) ReadAll(ctx context.Context, req *connect.Request[pb.ReadAllRequest]) (*connect.Response[pb.ReadAllResponse], error) {
@@ -72,13 +100,29 @@ func (s *server) ReadAll(ctx context.Context, req *connect.Request[pb.ReadAllReq
 		return nil, err
 	}
 
-	c := commands.NewReadAllCommand(s.Cache, s.Storage, s.Tracer)
-	res, err := c.Execute(ctx, req.Msg)
+	msg := req.Msg
+	userID := msg.GetUserId()
+	ctx, span := s.Tracer.Start(ctx, "ReadAll", trace.WithAttributes(attribute.String("userID", userID)))
+	defer span.End()
+
+	records, err := s.Storage.ReadAll(ctx, userID)
 	if err != nil {
-		return nil, err
+		telemetry.TraceError(span, err)
+		return nil, errors.HandleStorageError(err)
 	}
 
-	return connect.NewResponse(res), nil
+	posts := []*pb.ReadResponse{}
+	for _, record := range records {
+		posts = append(posts, &pb.ReadResponse{
+			UserId:    record.UserID,
+			PostId:    record.PostID,
+			Data:      record.Data,
+			CreatedAt: timestamppb.New(record.CreatedAt),
+			UpdatedAt: timestamppb.New(record.UpdatedAt),
+		})
+	}
+
+	return connect.NewResponse(&pb.ReadAllResponse{Posts: posts}), nil
 }
 
 func (s *server) Update(ctx context.Context, req *connect.Request[pb.UpdateRequest]) (*connect.Response[pb.UpdateResponse], error) {
@@ -86,13 +130,23 @@ func (s *server) Update(ctx context.Context, req *connect.Request[pb.UpdateReque
 		return nil, err
 	}
 
-	c := commands.NewUpdateCommand(s.Cache, s.Storage, s.Tracer)
-	res, err := c.Execute(ctx, req.Msg)
-	if err != nil {
-		return nil, err
-	}
+	msg := req.Msg
+	userID := msg.GetUserId()
+	postID := msg.GetPostId()
+	ctx, span := s.Tracer.Start(ctx, "Update", trace.WithAttributes(attribute.String("userID", userID), attribute.String("postID", postID)))
+	defer span.End()
 
-	return connect.NewResponse(res), nil
+	updatedAt, err := s.Storage.Update(ctx, userID, postID, msg.GetData())
+	if err != nil {
+		telemetry.TraceError(span, err)
+		return nil, errors.HandleStorageError(err)
+	}
+	s.Cache.Remove(ctx, userID, postID)
+
+	return connect.NewResponse(&pb.UpdateResponse{
+		PostId:    msg.PostId,
+		UpdatedAt: timestamppb.New(updatedAt),
+	}), nil
 }
 
 func (s *server) Delete(ctx context.Context, req *connect.Request[pb.DeleteRequest]) (*connect.Response[pb.DeleteResponse], error) {
@@ -100,11 +154,17 @@ func (s *server) Delete(ctx context.Context, req *connect.Request[pb.DeleteReque
 		return nil, err
 	}
 
-	c := commands.NewDeleteCommand(s.Cache, s.Storage, s.Tracer)
-	res, err := c.Execute(ctx, req.Msg)
-	if err != nil {
-		return nil, err
-	}
+	msg := req.Msg
+	userID := msg.GetUserId()
+	postID := msg.GetPostId()
+	ctx, span := s.Tracer.Start(ctx, "Delete", trace.WithAttributes(attribute.String("userID", userID), attribute.String("postID", postID)))
+	defer span.End()
 
-	return connect.NewResponse(res), nil
+	if err := s.Storage.Delete(ctx, userID, postID); err != nil {
+		telemetry.TraceError(span, err)
+		return nil, errors.HandleStorageError(err)
+	}
+	s.Cache.Remove(ctx, userID, postID)
+
+	return connect.NewResponse(&pb.DeleteResponse{}), nil
 }
