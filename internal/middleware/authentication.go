56d0f01b3994bcc84f5eaefbb3b1a4c4412e7ad2
Craig Pastro <pastro.craig@gmail.com>
2023-06-13 17:06:12 -0700

    Remove unused function

--- internal/middleware/authentication.go
+++ internal/middleware/authentication.go
@@ -14,10 +14,6 @@ type ctxKey string
 
 var userIDCtxKey = ctxKey("user-id-ctx-key")
 
-func SetUserIDInCtx(ctx context.Context, userID string) context.Context {
-	return context.WithValue(ctx, userIDCtxKey, userID)
-}
-
 func GetUserIDFromCtx(ctx context.Context) string {
 	userID := ctx.Value(userIDCtxKey).(string)
 	if userID == "" {
